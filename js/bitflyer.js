9819617b809bb84bdc6e8554d325695a835fcc6a
Gaurav <buddies2705@gmail.com>
2020-05-30 02:54:38 +0530

    Updating bitFlyer API endpoint
    The library is still using the old endpoint.
    
    The new API endpoint is  - api.bitflyer.com
    
    Ref - https://lightning.bitflyer.com/docs

--- js/bitflyer.js
+++ js/bitflyer.js
@@ -26,7 +26,7 @@ module.exports = class bitflyer extends Exchange {
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/28051642-56154182-660e-11e7-9b0d-6042d1e6edd8.jpg',
-                'api': 'https://api.bitflyer.jp',
+                'api': 'https://api.bitflyer.com',
                 'www': 'https://bitflyer.jp',
                 'doc': 'https://lightning.bitflyer.com/docs?lang=en',
             },
